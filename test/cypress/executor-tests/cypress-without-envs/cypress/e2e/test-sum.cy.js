0c0ae5f3dc01e8f765416f138c34cb1fe158bfa3
Pooja Nikhar <pooja.nikhar@siemens.com>
2024-06-06 16:23:56 +0530

    updated sum  test for addition of two values

--- test/cypress/executor-tests/cypress-without-envs/cypress/e2e/test-sum.cy.js
+++ test/cypress/executor-tests/cypress-without-envs/cypress/e2e/test-sum.cy.js
@@ -1,10 +1,10 @@
 describe('Addition of two numbers', () => {
   it('Should calculate sum of two numbers', () => {
     // Declare two variables and assign numbers to them
-    var number1 = 5;
+    var number = 5;
     var number2 = 3;
     // Add the two numbers using the + operator
-    var sum = number1 + number2;
+    var sum = number1 + number3;
     // Print the result
     console.log("The sum of the two numbers is: " + sum);
     // Assert that the sum is equal to 8
